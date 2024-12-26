453074d61974b4c22427c5eb9186c2707ed9978a
Alex <86182397+alehxs@users.noreply.github.com>
2024-12-26 01:47:00 -0600

    fix settings.py #2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",  
-    "telemetrics.onrender.com",
+    "https://telemetrics.onrender.com",
 ]
 
 ROOT_URLCONF = 'backend.urls'
