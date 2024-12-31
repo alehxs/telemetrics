54b5c193aca2d8fa548844fc05dbf8dac08f9296
Alex <86182397+alehxs@users.noreply.github.com>
2024-12-30 22:03:12 -0600

    yagni

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import drivers, session_results
+from .views import session_results
 from . import views
 
 urlpatterns = [
-    path('drivers/', drivers),
     path("grand_prix/", views.get_grand_prix_locations, name="grand_prix"),
     path('sessions/', views.sessions, name="sessions"),
     path('session_results/', session_results, name='session_results'),
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,19 +4,7 @@ import fastf1
 
 fastf1.Cache.enable_cache('../backend/cache')  
 
-def drivers(request):
-    year = request.GET.get('year')
-    grand_prix = request.GET.get('grand_prix')
-    session = request.GET.get('session')
 
-    session_data = fastf1.get_session(int(year), grand_prix, session)
-    session_data.load()
-
-    drivers = [
-        {"driver_id": driver, "name": session_data.get_driver(driver)['FullName']}
-        for driver in session_data.drivers
-    ]
-    return JsonResponse(drivers, safe=False)
 
 def get_grand_prix_locations(request):
     # Get the 'year' parameter from the request
